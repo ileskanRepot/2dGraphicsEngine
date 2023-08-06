5a2e9fcede20049847814c1ec86cf480fd905125
Ileska <ileska@tutanota.com>
2023-08-06 21:22:43 +0300

    Make list squares function

--- render.c
+++ render.c
@@ -4,7 +4,22 @@
 
 #include "structs.h"
 
+// int renderWorldSimple(struct world *map) {
+//     ;
+//   return 0;
+// }
+
 int listCubes(struct world *map) {
-  printf("Square count: %d\n", map->squares.count);
+  int squareCount = map->squares.count;
+  printf("Square count: %d\n", squareCount);
+
+  for (int ii = 0; ii < squareCount; ii++) {
+    printf("--- Square %d ---\n", ii);
+    printf("Position (%.2f, %.2f)\n", map->squares.arr[ii].position.x,
+           map->squares.arr[ii].position.y);
+
+    printf("Dimensions (%.2f, %.2f)\n", map->squares.arr[ii].dimension.w,
+           map->squares.arr[ii].dimension.h);
+  }
   return 0;
 }
