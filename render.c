64dcd42079487c9b77918ed17a5eb6a84201a8f7
Ileska <ileska@tutanota.com>
2023-08-15 18:48:58 +0300

    Make cube drawing possible

--- render.c
+++ render.c
@@ -4,10 +4,43 @@
 
 #include "structs.h"
 
-// int renderWorldSimple(struct world *map) {
-//     ;
-//   return 0;
-// }
+int isSquareInPixel(int x, int y, struct world *map) {
+  for (int ii = 0; ii < map->squares.count; ii++) {
+    struct dim dim = map->squares.arr[ii].dimension;
+    struct pos pos = map->squares.arr[ii].position;
+    if ((pos.x <= x && x < pos.x + dim.w) &&
+        (pos.y <= y && y < pos.y + dim.h)) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int renderWorldSimple(struct world *map) {
+  int squareCount = map->squares.count;
+  printf("%d\n", squareCount);
+  struct pos playerPos = map->camera.position;
+  struct dim playerDim = map->camera.dimension;
+  printf("  ");
+  for (int ww = -(playerDim.w / 2); ww <= (int)(playerDim.h / 2); ww++) {
+    printf("%2d", ww);
+  }
+  printf("\n");
+
+  for (int hh = -(playerDim.h / 2); hh <= (int)(playerDim.h / 2); hh++) {
+    printf("%2d", hh);
+    for (int ww = -(playerDim.w / 2); ww <= (int)(playerDim.h / 2); ww++) {
+      if (isSquareInPixel(playerPos.x + ww, playerPos.y + hh, map)) {
+        printf("##");
+      } else {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
 
 int listCubes(struct world *map) {
   int squareCount = map->squares.count;
--- render.h
+++ render.h
@@ -1,3 +1,4 @@
 #include "structs.h"
 
 int listCubes(struct world *map);
+int renderWorldSimple(struct world *map);
